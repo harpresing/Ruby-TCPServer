fabb93484a44b854d19fc3404e73248acc4f7e58
Omnigrass <harpreethighlyreactive@gmail.com>
2015-11-06 20:04:14 +0000

    Added command line arguments, corrected HELO message output

--- Server.rb
+++ Server.rb
@@ -3,44 +3,48 @@ require 'socket'
 require 'thread/pool'
 class Server
 
-  def initialize(host,ip)
-    @socket= TCPServer.new(host,ip)
+  def initialize(host,port)
+    @host=host
+    @port=port
+    @socket= TCPServer.open(@host,@port)
     @threadPool=Thread.pool(4) #There can be a maximum of 4 threads at a time
     @sock_domain 
     @remote_port
     @remote_hostname
     @remote_ip
+    @StudentID=ARGV[2]||152
   end
   
   def welcome(client)
     @sock_domain, @remote_port, @remote_hostname, @remote_ip = client.peeraddr
     puts "log: Connection from #{@remote_ip} at port #{@remote_port}"
-    client.puts "Server: Welcome #{@remote_hostname}\n"
   end
 
 
   def new_Connection(client)
-    while input=client.gets
+      while true
+      input=client.gets
       puts "log: got input from client"
       puts "From Client: #{input}"
       handle_Connection(input,client)
-    end
+      end
   end
 
   def handle_Connection(input,client)
-    if input=="Helo text\n"
-     client.puts "HELO text IP:[#{@remote_ip}] Port:[#{@remote_port}]\n"
-     elsif input=="KILL_SERVICE\n"
-     @socket.close
-     abort("Goodbye")
-     else
-     client.puts "Sorry buddy, not programmed to deal with that input yet\n"
+    if input[0,4]=="HELO"
+      client.puts "#{input}IP:#{@host}\nPort:#{@port}\nStudentID:#{@StudentID}\n"
+    elsif input=="KILL_SERVICE\n"
+      client.puts "Goodbye"
+      @socket.close
+      abort("Goodbye")
+    else
+      client.puts "Invalid Input \n"
     end
   end
     
   def run
-    puts "Starting up server..."
-    while true 
+    puts "Server running on Port #{@port} ..."
+    while true
       @threadPool.process {
       client=@socket.accept 
 	    welcome(client)
@@ -52,7 +56,7 @@ end
 
 if _FILE_=$0
 
-server = Server.new("Localhost", ARGV[0] || 420)
+server = Server.new(ARGV[0]||"Localhost",ARGV[1]||5000)
 server.run()
 
 end

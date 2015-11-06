3420967ce87656c926754b0073f6f7c50a21eafa
Omnigrass <harpreethighlyreactive@gmail.com>
2015-11-06 20:02:22 +0000

    Added functionality to accept multiline messages from server

--- Client.rb
+++ Client.rb
@@ -1,13 +1,34 @@
 require 'socket'
 
-s = TCPSocket.new("localhost",420)
+
+s = TCPSocket.new("localhost",ARGV[0] || 5000)
 puts "log: Starting Session \n"
 
 
 
-while !(s.closed?) && (LineFromServer=s.gets)
-	puts "From Server: #{LineFromServer}"
-	puts "Say something to Server"
-	s.puts $stdin.gets
-end
+	while !(s.closed?) 
+    	puts "Say something to Server"
+		l=$stdin.gets
+		s.puts l
+
+		if l[0,4]=="HELO"
+			puts "From Server: "
+			i=0
+			while(lineFromServer=s.gets)
+			 	puts "#{lineFromServer}"
+			 	i+=1
+			 	if(i==4)
+			 	   break
+				end
+			end
+    	else
+	    	lineFromServer=s.gets
+	    	puts "From Server: #{lineFromServer}"
+    	end
+    end
+
+
+
+
+
 

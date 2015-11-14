6a5953a3a143757b9f75d5757329a2d9370d8c5a
Omnigrass <harpreethighlyreactive@gmail.com>
2015-11-14 20:06:48 +0000

    Added username feature

--- Server.rb
+++ Server.rb
@@ -9,8 +9,10 @@ class Server
     @host=host
     @port=port
     @descriptors=Array.new #Stores all client sockets and the server socket
+    @clientName=Array.new
     @serverSocket= TCPServer.open(@host,@port)
     @descriptors.push(@serverSocket)
+    @client
     @threadPool=Thread.pool(4) #There can be a maximum of 4 threads at a time
     @sock_domain 
     @remote_port 
@@ -19,28 +21,49 @@ class Server
     @StudentID=ARGV[2]||152
   end
   
-  def welcome(client)
-    @sock_domain, @remote_port, @remote_hostname, @remote_ip = client.peeraddr
+  def welcome
+    @sock_domain, @remote_port, @remote_hostname, @remote_ip = @client.peeraddr
     puts "log: Connection from #{@remote_ip} at port #{@remote_port}"
-  end
-
-
-  def new_Connection(client)
-      while true
-      input=client.gets
-      puts "log: got input from client"
-      puts "From Client: #{input}"
-      handle_Connection(input,client)
+    flag=checkname
+      if flag==1 
+        checkname
       end
   end
 
-  def handle_Connection(input,client)
+  def checkname #checks if username is available
+    flag=0
+    input=@client.gets
+      @clientName.each do |name| #If username is already taken, flag is set to 1
+        if name==input              
+          flag=1
+        end
+      end
+      if flag==0
+        @clientName.push(input)
+        @client.puts "Success"
+      else
+        @client.puts "Username taken, try something else.."
+      end
+      return flag
+  end
+
+
+    def new_Connection
+      while true
+      input=@client.gets
+      puts "log: got input from client"
+      puts "From Client: #{input}"
+      handle_Connection(input)
+      end
+  end
+
+  def handle_Connection(input)
     if input[0,4]=="HELO"
-      client.puts "#{input}IP:#{@host}\nPort:#{@port}\nStudentID:#{@StudentID}\n"
+      @client.puts "#{input}IP:#{@host}\nPort:#{@port}\nStudentID:#{@StudentID}\n"
     elsif input=="KILL_SERVICE\n"
       terminate
     else
-      client.puts "Invalid Input \n"
+      @client.puts "Invalid Input \n"
     end
   end
 
@@ -63,10 +86,10 @@ class Server
     puts "Server running on Port #{@port} ..."
     while true
       @threadPool.process {
-      client=@serverSocket.accept 
-      @descriptors.push(client)
-	    welcome(client)
-      new_Connection(client)
+      @client=@serverSocket.accept 
+      @descriptors.push(@client)
+	    welcome
+      new_Connection
       }
     end
   end

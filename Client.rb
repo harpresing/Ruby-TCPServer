4e91423cbdb92de8a6b766420509185c29eb4d04
Omnigrass <harpreethighlyreactive@gmail.com>
2015-11-15 18:25:49 +0000

    added join functionality

--- Client.rb
+++ Client.rb
@@ -5,22 +5,40 @@ class Client
 def initialize(host,port) 
 	@s = TCPSocket.new(host, port)
 	@clientName
+	@chatroom
 	puts "log: Starting Session \n"
 end
 
-def join
-	puts "username:"
+def username
+	puts "Enter a username:"
 	@clientName= $stdin.gets
 	@s.puts @clientName
 	answer=@s.gets
 	puts answer
 	if answer[0,7]!="Success"
-		 join
+		 username
+	end
+end
+
+def sendJoinRequest
+	username
+	puts "Enter the name of the chatroom you would like to join"
+	@chatroom=$stdin.gets.chomp
+	@s.puts "JOIN_CHATROOM:#{@chatroom}\nClient_IP:\nPORT:\nCLIENT_NAME:#{@clientName}"
+end
+
+def getJoinResponse
+	i=0;
+	while i<=4
+		puts @s.gets
+		i+=1
 	end
 end
 
 def run
-   join
+    sendJoinRequest
+    getJoinResponse 
+	
 	while !(@s.closed?) 
     	puts "Say something to Server"
 		l=$stdin.gets
